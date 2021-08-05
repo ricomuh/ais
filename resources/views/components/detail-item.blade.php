e5c9ce5842bd6d674b3bf303d59d1502c5ce3cbe
ricointhehouseyo <rico.smartlolo@gmail.com>
2021-08-06 00:46:43 +0700

    detail item in staff page

---
+++ resources/views/components/detail-item.blade.php
@@ -0,0 +1,6 @@
+@props(['name','value'])
+
+<div class="flex flex-col divide-y divide-gray-200">
+    <div class="font-bold text-lg text-primary">{{ $name }}</div>
+    <div class="text-gray-800">{{ $value }}</div>
+</div>
