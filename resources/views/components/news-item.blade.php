ea4758e6f5caff3b6b1ef69cfef237a51d23b093
ricointhehouseyo <rico.smartlolo@gmail.com>
2021-08-05 14:44:05 +0700

    News Component

--- resources/views/components/news-item.blade.php
+++ resources/views/components/news-item.blade.php
@@ -1,13 +1,13 @@
 @props(['post', 'h' => 64, 'small' => false, 'class' => ''])
 
-<a href="{{ route('news.read', $post->slug) }}" class="group h-{{ $h }} w-full relative inline-flex shadow-md hover:shadow-lg transition duration-300 overflow-hidden transform hover:-translate-y-1 {{ $class }}">
+<a href="{{ route('news.read', $post->slug) }}" class="group h-{{ $h }} w-full relative inline-flex shadow-md hover:shadow-lg transition duration-300 overflow-hidden {{ $class }}">
     <img src="{{ asset('img/thumbnails/' . $post->thumbnail) }}" alt="{{ $post->title }}" class="w-full h-full object-cover transform group-hover:scale-110 transition duration-300">
     <div class="absolute w-full h-full bg-gradient-to-t from-black opacity-50 group-hover:opacity-70 transition duration-300"></div>
     <div class="absolute bottom-0 left-0 w-full p-4 text-white">
         <h4 class="font-semibold {{ $small ? 'text-sm' : 'text-base' }}">
             {{ $post->title }}
         </h4>
-        <p class="{{ $small ? 'text-xs' : 'text-sm' }} mt-2"><i class="fas fa-user"></i> {{ $post->user->name }} <i class="fas fa-calendar"></i> {{ $post->created_at->format('d F Y') }}</p>
+        <p class="{{ $small ? 'text-xs' : 'text-sm' }} mt-2"><i class="fas fa-user"></i> {{ $post->user->name }} <i class="fas fa-calendar ml-2"></i> {{ $post->created_at->format('d F Y') }}</p>
     </div>
     <div class="absolute bg-black group-hover:bg-primary-dark transition duration-300 right-0 top-4 text-white px-3 py-1 font-semibold {{ $small ? 'text-sm' : 'text-base' }}">{{ $post->tag->name }}</div>
 </a>
--- resources/views/components/popular-news.blade.php
+++ resources/views/components/popular-news.blade.php
@@ -1,10 +1,15 @@
 <div class="flex flex-col space-y-4">
+    <h2 class="text-lg font-bold text-primary">Search News</h2>
+    <form action="{{ route('news.search') }}" method="get" class="flex space-x-2">
+        <input type="text" name="query" id="search" placeholder="Search news..." class="px-2 py-1 bg-white hover:bg-gray-50 rounded-md border border-primary focus:border-primary-light focus:ring-2 focus:ring-opacity-50 focus:ring-primary focus:outline-none text-sm transition duration-300 flex-grow">
+        <button type="submit" class="bg-primary text-secondary hover:bg-primary-light hover:text-secondary-light focus:outline-none focus:ring-2 focus:ring-opacity-50 focus:ring-primary px-2 py-1 text-sm rounded-md transition duration-300">Search <i class="fas fa-search"></i></button>
+    </form>
     <h2 class="text-lg font-bold text-primary">Popular News</h2>
     <div class="flex flex-col">
         @foreach ($posts as $post)
             <a href="{{ route('news.read', $post->slug) }}" class="grid grid-cols-5 relative p-2 gap-2 group hover:bg-gray-100 hover:shadow-md transition duration-300">
                 <div class="col-span-2 h-full overflow-hidden">
-                    <img src="{{ asset('img/thumbnails/' . $post->thumbnail) }}" alt="" class="w-full h-full object-cover rounded">
+                    <img src="{{ asset('img/thumbnails/' . $post->thumbnail) }}" alt="" class="w-full h-full object-cover">
                 </div>
                 <div class="col-span-3 flex flex-col">
                     <div class="font-bold text-primary group-hover:text-primary-dark transition duration-300 leading-tight">{{ $post->title }}</div>
