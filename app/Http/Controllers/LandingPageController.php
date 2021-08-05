0c5af7e9f5ddef1603f22477605c903cb0a574cb
ricointhehouseyo <rico.smartlolo@gmail.com>
2021-08-05 09:59:08 +0700

    page read done

---
+++ app/Http/Controllers/LandingPageController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Page;
+use Illuminate\Http\Request;
+
+class LandingPageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Page  $page
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Page $page)
+    {
+        $page->views++;
+        $page->save();
+
+        return view('page', compact('page'));
+    }
+}
---
+++ resources/views/page.blade.php
@@ -0,0 +1,43 @@
+<x-main-layout>
+    <div class="bg-primary pt-8 border-b-4 border-secondary" id="header" style="background-image: url({{ asset('img/pattern1.png') }}); background-repeat: repeat-x; background-attachment: fixed;">
+        <div class="w-10/12 mx-auto flex flex-col py-16 space-y-16">
+            <div class="">
+                <ul class="flex text-sm lg:text-base">
+                    <li class="inline-flex items-center">
+                        <a href="{{ route('home') }}" class="text-secondary-dark">Home</a>
+                        <svg class="h-5 w-auto text-gray-400" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+                    </li>
+                    @if ($page->menu_id !== null)
+                        <li class="inline-flex items-center" >
+                            <a href="{{ route('home') }}" class="text-secondary-dark">{{ $page->menu->name }}</a>
+                            <svg class="h-5 w-auto text-gray-400" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+                        </li>
+                    @endif
+                    <li class="inline-flex items-center" >
+                        <div class="text-secondary">{{ $page->title }}</div>
+                    </li>
+                </ul>
+            </div>
+            <h1 class="text-3xl text-secondary font-bold tracking-widest">{{ $page->title }}</h1>
+        </div>
+    </div>
+    <div class="my-16 w-10/12 mx-auto leading-loose">
+        {!! $page->body !!}
+    </div>
+
+    @push('custom-script')
+        <script>
+            $(window).scroll(function() {
+                $("#header").css({
+                    "background-position-y" : -$(window).scrollTop()/2.2
+                });
+            });
+            @if ($page->menu_id !== null) {
+                $("#{{ $page->menu->slug }}")
+                    .removeClass("text-primary")
+                    .addClass("text-secondary bg-primary")
+            }
+            @endif
+        </script>
+    @endpush
+</x-main-layout>
