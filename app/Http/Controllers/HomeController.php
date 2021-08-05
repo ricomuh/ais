8d6b64e9939e661a207bb240bee46eb5d4cd43f2
ricointhehouseyo <rico.smartlolo@gmail.com>
2021-08-06 00:47:09 +0700

    change how long youtube api take in a day

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -20,10 +20,10 @@ class HomeController extends Controller
     public function __invoke(Request $request)
     {
         $carousels = Carousel::all();
-        $yt = cache()->remember('yt', 60 * 60 * 24, function () {
+        $yt = cache()->remember('yt', 60 * 60 * 6, function () {
             return Http::get('https://youtube.googleapis.com/youtube/v3/channels?part=snippet&part=statistics&id=UCcJ-ZCKCIDfmNFrOA6pC_Wg&key=AIzaSyDhViTqsxB6CRrj9P8mAvo1RPshbgI2uPI')->collect();
         });
-        $ytVideo = cache()->remember('ytVideo', 60 * 60 * 24, function () {
+        $ytVideo = cache()->remember('ytVideo', 60 * 60 * 6, function () {
             return Http::get('https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCcJ-ZCKCIDfmNFrOA6pC_Wg&maxResults=1&order=date&key=AIzaSyDhViTqsxB6CRrj9P8mAvo1RPshbgI2uPI')->collect();
         });
         $faqs = Faq::latest()->take(3)->get();
