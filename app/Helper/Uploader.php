2c9f2fed0b1787cb27ae008b78eb090cf379e831
ricointhehouseyo <rico.smartlolo@gmail.com>
2021-08-08 00:05:25 +0700

    changeable prefix

--- app/Helper/Uploader.php
+++ app/Helper/Uploader.php
@@ -6,9 +6,14 @@ use Illuminate\Support\Facades\File;
 
 class Uploader
 {
+    public static function prefix()
+    {
+        return 'deletable-';
+    }
+
     public static function upload($file,  $folder, $name = '')
     {
-        $filename = 'dlt-' . $name . '-' . rand(0, 9999999) . '.' . $file->getClientOriginalExtension();
+        $filename = self::prefix() . $name . '-' . rand(0, 9999999) . '.' . $file->getClientOriginalExtension();
         $file->move(public_path() . '/' . $folder, $filename);
 
         return $filename;
@@ -16,7 +21,7 @@ class Uploader
 
     public static function deleteWhenExist($filename, $folder)
     {
-        if (substr($filename, 0, 4) == 'dlt-') {
+        if (substr($filename, 0, strlen(self::prefix())) == self::prefix()) {
             $filepath = public_path() . '/' . $folder . '/' . $filename;
             if (File::exists($filepath)) {
                 File::delete($filepath);
