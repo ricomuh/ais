61bc2de364e722866e8b16e3b0d7afcda64be323
ricointhehouseyo <rico.smartlolo@gmail.com>
2021-08-07 21:29:20 +0700

    Add animation

--- resources/views/components/dashboard-textarea.blade.php
+++ resources/views/components/dashboard-textarea.blade.php
@@ -1,3 +1,3 @@
 @props(['name', 'value' => ''])
 
-<textarea name="{{ $name }}" id="{{ $name }}" class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" cols="60" rows="2">{{ $value }}</textarea>
+<textarea name="{{ $name }}" id="{{ $name }}" class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50  transition duration-300" cols="60" rows="1">{{ $value }}</textarea>
