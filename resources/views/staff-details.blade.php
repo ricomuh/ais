f8f3a37af721a2e777d3f26f308c6e17af71f5a2
ricointhehouseyo <rico.smartlolo@gmail.com>
2021-08-08 12:47:08 +0700

    Center the staff image in staff details

--- resources/views/staff-details.blade.php
+++ resources/views/staff-details.blade.php
@@ -6,8 +6,8 @@
         <a href="{{ route('staff.index') }}" class="text-lg font-bold text-primary hover:text-primary-light"><i class="fas fa-arrow-left"></i> Back</a>
     </div>
     <div class="flex flex-col md:flex-row gap-8">
-        <div class="w-full md:w-1/4 animate" data-animate="animate-from-left">
-            <img src="{{ asset('img/staff/' . $staff->photo) }}" alt="" class="w-full rounded-full shadow-md border-4 border-secondary">
+        <div class="w-full md:w-1/4 animate flex justify-center" data-animate="animate-from-left">
+            <img src="{{ asset('img/staff/' . $staff->photo) }}" alt="" class="md:w-48 md:h-48 object-cover rounded-full shadow-md border-4 border-secondary">
         </div>
         <div class="w-full md:flex-shrink flex flex-col space-y-4 animate" data-animate="animate-from-right">
             @foreach (
