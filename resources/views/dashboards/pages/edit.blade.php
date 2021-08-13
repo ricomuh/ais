db44928a888876399db6d86b2ec16b50ff23036b
ricointhehouseyo <rico.smartlolo@gmail.com>
2021-08-13 13:35:29 +0700

    HTML editor

--- resources/views/dashboards/pages/edit.blade.php
+++ resources/views/dashboards/pages/edit.blade.php
@@ -11,7 +11,18 @@
             </x-dashboard-select>
         </x-dashboard-auto-custom>
         <x-label for="body">Content</x-label>
-        <x-dashboard-texteditor name="body" :value="$page->body" />
+        <div class="flex flex-col" x-data="{ html : false }">
+            <div class="flex flex-row justify-start">
+                <div class="px-4 py-2 border border-gray-300 cursor-pointer text-gray-700 rounded-t-md transition duration-300 border-b-0 hover:bg-gray-100" @click="html = false" :class="{'bg-gray-200 transform scale-90 translate-y-0.5 ' : html}">Plain Text</div>
+                <div class="px-4 py-2 border border-gray-300 cursor-pointer text-gray-700 rounded-t-md transition duration-300 border-b-0 hover:bg-gray-100" @click="html = true; openHtml();" :class="{'bg-gray-200 transform scale-90 translate-y-0.5 ' : !html}" >Html</div>
+            </div>
+            <div x-show="!html">
+                <x-dashboard-texteditor name="body" :value="$page->body" />
+            </div>
+            <div x-show="html">
+                <textarea name="html" id="html" class="w-full h-96" placeholder="Html..." x-ref="htmlEditor"></textarea>
+            </div>
+        </div>
         <div class="flex justify-center">
             <x-button>Save</x-button>
         </div>
@@ -21,6 +32,12 @@
             $("#title, #slug").change(function(){
                 var value = $(this).val().toLowerCase().replace(/ /g,'-').replace(/[^\w-]+/g,'')
                 $("#slug").val(value);
+            });
+            function openHtml() {
+                $("#html").val(tinyMCE.get('body').getContent());
+            }
+            $("#html").keyup(function(){
+                tinyMCE.get("body").setContent($(this).val());
             });
         </script>
     @endpush
