4068a8cc98b48f1facb764787c340539b091ffa2
ricointhehouseyo <rico.smartlolo@gmail.com>
2021-08-24 19:23:35 +0700

    Staff Details Translated to Bahasa

--- resources/views/staff-details.blade.php
+++ resources/views/staff-details.blade.php
@@ -10,14 +10,23 @@
             <img src="{{ asset('img/staff/' . $staff->photo) }}" alt="" class="md:w-48 md:h-48 object-cover rounded-full shadow-md border-4 border-secondary">
         </div>
         <div class="w-full md:flex-shrink flex flex-col space-y-4 animate" data-animate="animate-from-right">
-            @foreach (
+            {{-- @foreach (
             collect($staff)->filter(
                 function($value, $key){
                     return ($key !== 'id' && $key !== 'photo' && $key !== 'created_at' && $key !== 'updated_at');
                 }
             ) as $name => $value)
                 <x-detail-item :name="\Str::of($name)->replace('_',' ')->title()" :value="$value" />
-            @endforeach
+            @endforeach --}}
+            <x-detail-item name="Nama" :value="$staff->name" />
+            <x-detail-item name="Email" :value="$staff->email" />
+            <x-detail-item name="Jabatan Struktural" :value="$staff->structural_role" />
+            <x-detail-item name="Jabatan Fungsional" :value="$staff->functional_role" />
+            <x-detail-item name="Alamat" :value="$staff->address" />
+            <x-detail-item name="Edukasi Formal" :value="$staff->formal_educations" />
+            <x-detail-item name="Edukasi Nonformal" :value="$staff->nonformal_educations" />
+            <x-detail-item name="Pengalaman" :value="$staff->experiences" />
+            <x-detail-item name="Lain-lain" :value="$staff->publications" />
         </div>
     </div>
 </x-page-layout>
